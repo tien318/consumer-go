b271630a239c0a13ac689e63e1b075293466fea7
Nguyen Vu <nguyenvanvu028@gmail.com>
2017-10-03 10:22:44 +0700

    Update

--- cmd/sync-contact/main.go
+++ cmd/sync-contact/main.go
@@ -6,9 +6,16 @@ import (
 	"log"
 	"time"
 
+	"beeketing.com/beeketing-consumer-go/config"
+
 	_ "github.com/go-sql-driver/mysql"
+
 	"github.com/spf13/viper"
 )
+
+func init() {
+	config.Load()
+}
 
 func main() {
 	fmt.Println("Hello")
