9cb1cd59c6b6847f0ef41eccf33566b2138df50c
Nguyen Vu <nguyenvanvu028@gmail.com>
2017-10-03 10:39:15 +0700

    Update

--- cmd/sync-contact/main.go
+++ cmd/sync-contact/main.go
@@ -4,6 +4,8 @@ import (
 	"database/sql"
 	"fmt"
 	"log"
+	"net/http"
+	"net/url"
 	"time"
 
 	"beeketing.com/beeketing-consumer-go/config"
@@ -13,30 +15,50 @@ import (
 	"github.com/spf13/viper"
 )
 
+var db *sql.DB
+
 func init() {
 	config.Load()
 }
 
 func main() {
-	fmt.Println("Hello")
-
-	// Connect to mysql
-	db, err := sql.Open("mysql", viper.GetString("mysql.dsn"))
-	if err != nil {
-		log.Fatal(err)
-	}
+	fmt.Println("sync contact")
+	initMysql()
 	defer db.Close()
+
+	emails := getEmails(9561507)
+
+	for _, email := range emails {
+		_, err := http.PostForm("https://a.klaviyo.com/api/v1/list/LrnK26/members", url.Values{
+			"api_key":       {"pk_56a51680ebf1a3d5cc93ba6a82ecfe7ecb"},
+			"email":         {email},
+			"confirm_optin": {"false"},
+		})
+
+		if err != nil {
+			panic(err)
+		}
+
+		fmt.Println(email)
+	}
+}
+
+func initMysql() {
+	db, _ = sql.Open("mysql", viper.GetString("mysql.dsn"))
 
 	db.SetConnMaxLifetime(time.Second * 20)
 	db.SetMaxIdleConns(30)
 	db.SetMaxOpenConns(30)
 
-	err = db.Ping()
+	err := db.Ping()
 	if err != nil {
 		log.Fatal(err)
 	}
-	query := "select email from contacts where shop_id = 9561507 and (tracker_type = 'subscriber' or is_subscribed_via_cbox = true);"
-	rows, err := db.Query(query)
+}
+
+func getEmails(shopID int) []string {
+	query := "select email from contacts where shop_id = ? and (tracker_type = 'subscriber' or is_subscribed_via_cbox = true);"
+	rows, err := db.Query(query, shopID)
 	if err != nil {
 		panic(err)
 	}
@@ -54,5 +76,5 @@ func main() {
 		emails = append(emails, email)
 	}
 
-	fmt.Println(emails)
+	return emails
 }
