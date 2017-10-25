90a3c6d8e45bcb3e3e400ddb0a861723af720205
Nguyen Vu <nguyenvanvu028@gmail.com>
2017-10-25 15:45:02 +0700

    Update time

--- cmd/pusher/main.go
+++ cmd/pusher/main.go
@@ -76,8 +76,8 @@ func main() {
 
 	c := cron.New()
 
-	log.Info("run every 1m")
-	c.AddFunc("@every 1m", func() {
+	log.Info("run every 5m")
+	c.AddFunc("@every 5m", func() {
 		run()
 	})
 
@@ -90,8 +90,8 @@ func run() {
 	log.Info("Start Fetch Abandoned Checkout from Shopify")
 
 	// init time
-	updatedAtMin := time.Now().Add(-time.Minute * 60).Format(time.RFC3339)
-	updatedAtMax := time.Now().Add(-time.Minute * 0).Format(time.RFC3339)
+	updatedAtMin := time.Now().Add(-time.Minute * 20).Format(time.RFC3339)
+	updatedAtMax := time.Now().Add(-time.Minute * 15).Format(time.RFC3339)
 	// log.Infof("Time: %s - %s", updatedAtMin, updatedAtMax)
 
 	// get appshops
